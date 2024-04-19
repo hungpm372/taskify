154000ece73ccecb49b6dacef70897f0fc41c2c3
hungpm372 <hungpm372@gmail.com>
2024-04-20 00:36:49 +0700

    Edit metadata

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  title: 'Taskify',
+  description:
+    'Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify'
+}
