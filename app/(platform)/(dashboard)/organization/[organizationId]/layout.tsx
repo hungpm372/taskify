7c9fd83833f4f2f8142a840b1d3a46689d581d76
hungpm372 <hungpm372@gmail.com>
2024-04-22 22:52:34 +0700

    Update org metadata

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import OrgControl from './_components/OrgControl'
+import { startCase } from 'lodash'
+import { auth } from '@clerk/nextjs'
+
+export async function generateMetadata() {
+  const { orgSlug } = auth()
+
+  return {
+    title: startCase(orgSlug || 'Organization')
+  }
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
